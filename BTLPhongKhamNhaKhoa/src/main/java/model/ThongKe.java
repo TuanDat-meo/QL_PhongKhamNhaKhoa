abcd1e9376cd80e42f73df8410dcbaee579f0d3b
snsnj <6451071014@st.utc2.edu.vn>
2025-05-24 23:59:18 +0700

    updat

---
+++ BTLPhongKhamNhaKhoa/src/main/java/model/ThongKe.java
@@ -0,0 +1,61 @@
+package model;
+
+public class ThongKe {
+    private int soLuongVatTu;
+    private int soLuongNhaCungCap;
+    private int soLuongBenhNhan;
+    private int soLuongLichHen;
+    // Thêm các thuộc tính khác bạn muốn thống kê
+
+    public ThongKe() {
+    }
+
+    public ThongKe(int soLuongVatTu, int soLuongNhaCungCap, int soLuongBenhNhan, int soLuongLichHen) {
+        this.soLuongVatTu = soLuongVatTu;
+        this.soLuongNhaCungCap = soLuongNhaCungCap;
+        this.soLuongBenhNhan = soLuongBenhNhan;
+        this.soLuongLichHen = soLuongLichHen;
+    }
+
+    public int getSoLuongVatTu() {
+        return soLuongVatTu;
+    }
+
+    public void setSoLuongVatTu(int soLuongVatTu) {
+        this.soLuongVatTu = soLuongVatTu;
+    }
+
+    public int getSoLuongNhaCungCap() {
+        return soLuongNhaCungCap;
+    }
+
+    public void setSoLuongNhaCungCap(int soLuongNhaCungCap) {
+        this.soLuongNhaCungCap = soLuongNhaCungCap;
+    }
+
+    public int getSoLuongBenhNhan() {
+        return soLuongBenhNhan;
+    }
+
+    public void setSoLuongBenhNhan(int soLuongBenhNhan) {
+        this.soLuongBenhNhan = soLuongBenhNhan;
+    }
+
+    public int getSoLuongLichHen() {
+        return soLuongLichHen;
+    }
+
+    public void setSoLuongLichHen(int soLuongLichHen) {
+        this.soLuongLichHen = soLuongLichHen;
+    }
+
+    @Override
+    public String toString() {
+        return "ThongKe{" +
+                "soLuongVatTu=" + soLuongVatTu +
+                ", soLuongNhaCungCap=" + soLuongNhaCungCap +
+                ", soLuongBenhNhan=" + soLuongBenhNhan +
+                ", soLuongLichHen=" + soLuongLichHen +
+                '}';
+    }
+}
